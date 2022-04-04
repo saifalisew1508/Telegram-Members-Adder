2e231e816b8ed8dd85852f2c0675d755b1f513ab
saifalisew1508 <80207551+saifalisew1508@users.noreply.github.com>
2022-04-04 07:59:43 +0530

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,6 +1,6 @@
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
-import pickle, os, sys
+import pickle, os
 from colorama import init, Fore
 from time import sleep
 
