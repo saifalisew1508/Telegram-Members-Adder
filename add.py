4224d50b09aefb01c3e0195eed388d848ab8d897
Saif Ali <sachinsaif03@gmail.com>
2023-12-02 12:17:01 +0530

    Update add.py

--- add.py
+++ add.py
@@ -56,8 +56,9 @@ def banner():
     ]
     for char in b:
         print(f'{random.choice(colors)}{char}{rs}')
-    print('===========TELEGRAM MEMBERS ADDER============')
-    print(f'{lg}Version: {w}2.0{lg} | Author: {w}saifalisew1508{rs}')
+    print('Contact below address for get premium script')
+    print(f'{lg}Version: {w}2.0{lg} | GitHub: {w}@saifalisew1508{rs}')
+    print(f'{lg}Telegram: {w}@DearSaif{lg} | Instagram: {w}@_Prince.Babu_{rs}')
 
 
 # function to clear screen
